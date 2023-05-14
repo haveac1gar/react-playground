256aedd2d3b60356aba0c964a00bf40cd2c494c4
a.gulbe <alexander@aldrin.com>
2023-05-15 02:04:55 +0300

    example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-const App = () => {
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import { Settings } from "./pages/Settings";
+import { Header } from "./components/Header";
+
+function App() {
   return (
     <>
-      Hello motherfuca
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+        <Route path="/profile" element={<Profile />} ></Route>
+        <Route path="/settings" element={<Settings />} ></Route>
+      </Routes>      
     </>
-  )
-};
+  );
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import {
+  BrowserRouter,
+} from "react-router-dom";
+  
 const element = document.getElementById('root');
 
 if (element) {
   const root = ReactDOM.createRoot(element);
-  root.render(<App />);
+  root.render(
+    <BrowserRouter>
+      <App />а
+    </BrowserRouter>
+  );
 }
